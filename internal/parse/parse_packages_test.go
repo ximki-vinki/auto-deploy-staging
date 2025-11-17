bd331d04e0629019a6c2e6cfe17d91b0780f86c0
ILJA MANYUTIN <i.manytuin@auchan.ru>
2025-11-17 16:09:23 +0300

    TDD-2 red

--- internal/parse/parse_packages_test.go
+++ internal/parse/parse_packages_test.go
@@ -1,14 +1,23 @@
 package parse
 
 import (
-	"auto-deploy-staging/internal/domain"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestParsePackages_ReturnsCorrectType(t *testing.T) {
-	packages, _ := ParsePackages(nil)
+func TestParsePackages_OnePackage(t *testing.T) {
+	yamlData := []byte(`
+pim-pim:
+  repository: git@git.test.ru:auchan/pim/pim/pim.git
+`)
 
-	assert.Equal(t, []domain.Package{}, packages)
+	packages, _ := ParsePackages(yamlData)
+
+	require.Len(t, packages, 1)
+
+	pkg := packages[0]
+	assert.Equal(t, "pim-pim", pkg.Name)
+	assert.Equal(t, "git@git.test.ru:auchan/pim/pim/pim.git", pkg.Repository)
 }
