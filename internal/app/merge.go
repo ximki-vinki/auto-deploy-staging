9adcf8793f037ec92dfb23adc2959ffb9a28f9ab
ILJA MANYUTIN <i.manytuin@auchan.ru>
2025-11-18 17:53:24 +0300

    Добавить ENV в игнорируемые файлы

--- internal/app/merge.go
+++ internal/app/merge.go
@@ -5,8 +5,13 @@ import (
 	"auto-deploy-staging/internal/parse"
 	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 )
+
+const gitLabURL = "https://git.greensight.ru/api/v4/projects/auchan%2Fdevops%2Fdeveloper-pipelines/repository/files/README.md/raw?ref=master"
+const token = "glpat-yGP2XiqqJY5JGHjD6Xz62m86MQp1OjRlCA.01.0y02iiljw"
 
 func Merge() []domain.Service {
 	dataConfig, err := os.ReadFile("config.json")
@@ -57,4 +62,32 @@ func Convert(settings []domain.SettingService, packages map[string]domain.Packag
 		result = append(result, service)
 	}
 	return result
+}
+
+func fetchFromGitLab() ([]byte, error) {
+	url := os.Getenv("GITLAB_GET_README")
+	token := os.Getenv("GITLAB_PRIVATE_TOKEN")
+	if url == "" {
+		return nil, fmt.Errorf("GITLAB_GET_README is not set")
+	}
+	if token == "" {
+		return nil, fmt.Errorf("GITLAB_PRIVATE_TOKEN is not set")
+	}
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("PRIVATE-TOKEN", token)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("got HTTP %d", resp.StatusCode)
+	}
+
+	return io.ReadAll(resp.Body)
 }
